0fe31c0af2ecd64c1acfdad4c038da34c312889c
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-11-08 18:53:53 +0300

    fix

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -29,52 +29,52 @@ rellaxTargetsText.forEach(target => {
         });
     }
 })
-if (rellaxTargetsText.length) {
+// if (rellaxTargetsText.length) {
 
-    function transform(target, topPropertyValue, topGapTarget, rellaxName) {
-        const title = target.previousElementSibling;
-        const targetPosition = {
-            top: window.pageYOffset + title.getBoundingClientRect().top,
-            bottom: window.pageYOffset + title.getBoundingClientRect().bottom
-        }
-        const windowPosition = {
-            top: window.pageYOffset,
-            bottom: window.pageYOffset + document.documentElement.clientHeight
-        };
-        if (window.innerWidth > 1200) {
-            if (targetPosition.top < windowPosition.bottom - (window.innerHeight / 1.65) && targetPosition.bottom > windowPosition.top - target.clientHeight - 50) {
-                target.classList.remove('_translate-none');
-                const currentTopGapTarget = windowPosition.top + target.getBoundingClientRect().top;
-                const distinction = currentTopGapTarget - topGapTarget;
-                if (localStorage.getItem(rellaxName)) target.style.top = localStorage.getItem(rellaxName);
-                if (distinction !== NaN && (distinction < 0 || distinction >= 0) && distinction > -50 && distinction < 50) {
-                    localStorage.setItem(rellaxName, `${Math.round(topPropertyValue - distinction + 1)}px`)
-                    target.style.top = localStorage.getItem(rellaxName);
-                }
-            } else {
-                target.classList.add('_translate-none');
-                target.style.removeProperty('top');
-                return window.pageYOffset + target.getBoundingClientRect().top;
-            }
-        }
-    }
-
-
-    rellaxTargetsText.forEach(target => {
-        if (!target.classList.contains('_no-rellax')) {
-            if (window.innerWidth > 1200) {
-                const topPropertyValue = Number(window.getComputedStyle(target).getPropertyValue('top').replace('px', ''));
-                let topGapTarget;
-                topGapTarget = transform(target, topPropertyValue, topGapTarget, target.dataset.rellaxName);
-                window.addEventListener('scroll', () => {
-                    topGapTarget = transform(target, topPropertyValue, topGapTarget, target.dataset.rellaxName);
-                });
-            }
-        }
-    })
+//     function transform(target, topPropertyValue, topGapTarget, rellaxName) {
+//         const title = target.previousElementSibling;
+//         const targetPosition = {
+//             top: window.pageYOffset + title.getBoundingClientRect().top,
+//             bottom: window.pageYOffset + title.getBoundingClientRect().bottom
+//         }
+//         const windowPosition = {
+//             top: window.pageYOffset,
+//             bottom: window.pageYOffset + document.documentElement.clientHeight
+//         };
+//         if (window.innerWidth > 1200) {
+//             if (targetPosition.top < windowPosition.bottom - (window.innerHeight / 1.65) && targetPosition.bottom > windowPosition.top - target.clientHeight - 50) {
+//                 target.classList.remove('_translate-none');
+//                 const currentTopGapTarget = windowPosition.top + target.getBoundingClientRect().top;
+//                 const distinction = currentTopGapTarget - topGapTarget;
+//                 if (localStorage.getItem(rellaxName)) target.style.top = localStorage.getItem(rellaxName);
+//                 if (distinction !== NaN && (distinction < 0 || distinction >= 0) && distinction > -50 && distinction < 50) {
+//                     localStorage.setItem(rellaxName, `${Math.round(topPropertyValue - distinction + 1)}px`)
+//                     target.style.top = localStorage.getItem(rellaxName);
+//                 }
+//             } else {
+//                 target.classList.add('_translate-none');
+//                 target.style.removeProperty('top');
+//                 return window.pageYOffset + target.getBoundingClientRect().top;
+//             }
+//         }
+//     }
 
 
-}
+//     rellaxTargetsText.forEach(target => {
+//         if (!target.classList.contains('_no-rellax')) {
+//             if (window.innerWidth > 1200) {
+//                 const topPropertyValue = Number(window.getComputedStyle(target).getPropertyValue('top').replace('px', ''));
+//                 let topGapTarget;
+//                 topGapTarget = transform(target, topPropertyValue, topGapTarget, target.dataset.rellaxName);
+//                 window.addEventListener('scroll', () => {
+//                     topGapTarget = transform(target, topPropertyValue, topGapTarget, target.dataset.rellaxName);
+//                 });
+//             }
+//         }
+//     })
+
+
+// }
 
 popup(null, 'show-qr');
 
@@ -82,6 +82,9 @@ popup(null, 'show-qr');
 
 // ========================================================================================
 
-GreenAudioPlayer.init({
-    selector: '.custom-audio__wrapper',
-});
+if (document.querySelector('.custom-audio__wrapper')) {
+    GreenAudioPlayer.init({
+        selector: '.custom-audio__wrapper',
+    });
+    
+}
