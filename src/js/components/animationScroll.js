d077f2cee1336259340ed859e90748a001f1c0ac
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2023-12-14 13:44:19 +0300

    fix

--- src/js/components/animationScroll.js
+++ src/js/components/animationScroll.js
@@ -7,26 +7,36 @@ const animationScroll = () => {
         function animOnScroll() {
             for (let index = 0; index < items.length; index++) {
                 const animItem = items[index];
-                const animItemHeight = animItem.offsetHeight;
-                const animItemOffset = offset(animItem).top;
-
-                const animStartDefault = 4;
-                const animDelay = animItem.dataset.animationItemDelay ? animItem.dataset.animationItemDelay : 0;
-                const animStart = animItem.dataset.animationItemStart ? animItem.dataset.animationItemStart : animStartDefault;
-
-                let animItemPoint = window.innerHeight - animItemHeight / animStart;
-                if (animItemHeight > window.innerHeight) animItemPoint = window.innerHeight - window.innerHeight / animStart;
-
-                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                    setTimeout(() => {
-                        animItem.classList.add('_animation-active');
-                    }, animDelay);
+                const animBreak = animItem.dataset.animationItemMaxBreak;
+                if (animBreak) {
+                    if (window.innerWidth > animBreak) {
+                        body(animItem);
+                    }
                 } else {
-                    if (animItem.hasAttribute('data-animation-item-repeat')) animItem.classList.remove('_animation-active');
+                    body(animItem);
                 }
             }
         }
 
+        function body(animItem) {
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+
+            const animStartDefault = 4;
+            const animDelay = animItem.dataset.animationItemDelay ? animItem.dataset.animationItemDelay : 0;
+            const animStart = animItem.dataset.animationItemStart ? animItem.dataset.animationItemStart : animStartDefault;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) animItemPoint = window.innerHeight - window.innerHeight / animStart;
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                setTimeout(() => {
+                    animItem.classList.add('_animation-active');
+                }, animDelay);
+            } else {
+                if (animItem.hasAttribute('data-animation-item-repeat')) animItem.classList.remove('_animation-active');
+            }
+        }
         function offset(el) {
             const rect = el.getBoundingClientRect(),
                 scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
